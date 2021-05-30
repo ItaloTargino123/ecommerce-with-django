d272114c66a53935480196fd450ef39811a8fd59
Italo <italopardal@hotmail.com>
2021-05-30 17:21:59 +0100

    product image field

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import NullBooleanField
 # Create your models here.
 
 
@@ -17,10 +18,18 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
-    # image
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ""
+        return url
 
 
 class Order(models.Model):
