a04578d22e04488b7cac391b2cdb3b99106a2aeb
Italo <italopardal@hotmail.com>
2021-05-31 15:43:10 +0100

    update item view logic

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,40 +1,38 @@
-var updateBtns = document.getElementsByClassName("update-cart")
 
+var updateBtns = document.getElementsByClassName('update-cart')
 
-for (var i = 0; i < updateBtns.length; i++) {
-    updateBtns[i].addEventListener("click", function () {
+for (i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
         var productId = this.dataset.product
         var action = this.dataset.action
-        console.log("productId:", productId, "action:", action)
+        console.log('productId:', productId, 'Action:', action)
+        console.log('USER:', user)
 
-        console.log("User:", user)
-        if (user === "AnonymousUser") {
-            console.log("Not logged in")
-        }
-        else {
+        if (user == 'AnonymousUser') {
+            addCookieItem(productId, action)
+        } else {
             updateUserOrder(productId, action)
         }
     })
 }
 
-function updateUserOrder(productcId, action) {
-    console.log("User is logged in, sending data...")
+function updateUserOrder(productId, action) {
+    console.log('User is authenticated, sending data...')
 
-    var url = "/update_item/"
+    var url = '/update_item/'
 
     fetch(url, {
-        method: "POST",
+        method: 'POST',
         headers: {
-            "Content-Type": "application/json",
-            "X-CSRFToken": csrftoken,
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
         },
-        body: JSON.stringify({ "productId": productId, "action": action })
+        body: JSON.stringify({ 'productId': productId, 'action': action })
     })
         .then((response) => {
-            return response.json()
+            return response.json();
         })
-
-        .then((response) => {
-            console.log("data:", data)
-        })
+        .then((data) => {
+            location.reload()
+        });
 }
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 from .models import *
 
 # Create your views here.
@@ -41,4 +42,10 @@ def checkout(request):
 
 
 def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('Product:', productId)
     return JsonResponse("Item was added", safe=False)
